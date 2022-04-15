83a5aab04bbf25de0940bcf06d5504cea447b03c
Nathan-Savonen <85711523+Nathan-Savonen@users.noreply.github.com>
2022-04-14 23:09:01 -0700

    Update unlit.textured.fs.glsl

--- Week 10 - Texturing, Transparency/shaders/unlit.textured.fs.glsl
+++ Week 10 - Texturing, Transparency/shaders/unlit.textured.fs.glsl
@@ -9,10 +9,14 @@ uniform float uAlpha;
 varying vec2 vTexcoords;
 
 void main(void) {
-    // todo #5
 
+   vec4 texture = texture2D(uTexture,vTexcoords);
+    gl_FragColor = vec4(texture.x,texture.y,texture.z,uAlpha);
+    
+    // todo #5
+    //gl_FragColor = texture2D(uTexture,vTexcoords);
     // todo #3
-    gl_FragColor = vec4(vTexcoords, 0.0, uAlpha);
+    //gl_FragColor = vec4(vTexcoords, 0.0, uAlpha);
 }
 
 // EOF 00100001-10
